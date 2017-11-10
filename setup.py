77c8abe473dcb2f4beef11e22e26cc2301661fd7
Omar Aguilar <omar.aguilar@gmail.com>
2017-11-10 15:22:22 -0700

    Update setup.py
    Updating url for setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=['django_google_storage', ],
     author='Maxim Smirnoff',
     author_email='smirnoffmg@gmail.com',
-    url='https://github.com/budurli/django-google-storage',
+    url='https://github.com/omaraf/django-google-storage',
     license='LICENSE.txt',
     description='Django storage for Google Storage',
     install_requires=[
